2d08a408acfa390d09bda239a7310ad561a15e38
Hikmet Can Öyke <hikmetcanoyke@outlook.com>
2023-03-16 16:36:46 +0300

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
-import { useFont } from "expo-font";
+import { useFont, useFonts } from "expo-font";
 
 import Home from "./screens/Home";
 import Details from "./screens/Details";
 
+const Stack = createStackNavigator();
+
+const theme = {
+  ...DefaultTheme,
+  color: {
+    ...DefaultTheme.colors,
+    background: "transparent",
+  },
+};
+
 const App = () => {
-  const Stack = createStackNavigator();
+  const[loaded] =useFonts({
+    InterBold : require("./assets/fonts/Inter-Bold.ttf"),
+    InterSemiBold : require("./assets/fonts/Inter-SemiBold.ttf"),
+    InterMedium : require("./assets/fonts/Inter-Medium.ttf"),
+    InterRegular : require("./assets/fonts/Inter-Regular.ttf"),
+    InterLight : require("./assets/fonts/Inter-Light.ttf"),
+  })
   
-  const theme = {
-    ...DefaultTheme,
-    color:{
-      ...DefaultTheme.colors,
-      background:"transparent"
-    }
-  }
+  if(!loaded) return null;
+
 
   return (
     <NavigationContainer theme={theme}>
